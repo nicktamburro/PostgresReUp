4b2c91f44fde46e679f7d133e179bc99cd98f98a
nicktamburro <nicholastamburro@gmail.com>
2019-03-30 19:51:17 -0700

    hello again postgresql

---
+++ one.sql
@@ -0,0 +1,8 @@
+SELECT * FROM band;
+#just like mysql
+
+SELECT bandname, city, genre FROM band;
+#to narrow it down a little
+
+SELECT bandname, city, genre FROM band ORDER BY bandname ASC;
+#alpha by bandname
