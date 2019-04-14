044e34b990534f7c33bdfa882c1ec0fafbe18b7d
nicktamburro <nicholastamburro@gmail.com>
2019-04-13 21:43:00 -0700

    postgres

--- one.sql
+++ one.sql
@@ -6,3 +6,10 @@ SELECT bandname, city, genre FROM band;
 
 SELECT bandname, city, genre FROM band ORDER BY bandname ASC;
 #alpha by bandname
+
+#special PGS thing: PostgreSQL allows you to sort rows based on the 
+#columns that even does not appear in the selection list.
+#so far almost nothing about this is different than mysql,
+#
+#that's the good news'
+
